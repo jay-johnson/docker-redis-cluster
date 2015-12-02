9741c0b0844b50d651bf0c0061017dc16b6af3d8
jay-johnson <jay.p.h.johnson@gmail.com>
2015-12-01 21:20:12 -0500

    removing comment

--- start_cluster.sh
+++ start_cluster.sh
@@ -3,7 +3,6 @@
 ./cleanup_existing_cluster_containers.sh 
 
 echo "Starting the Cluster on Docker Swarm"
-#docker-compose up -d
 export DOCKER_HOST=localhost:4000
 docker-compose --x-networking --x-network-driver overlay up -d
 echo "Done"
